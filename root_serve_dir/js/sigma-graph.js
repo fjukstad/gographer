808a0d20961f3c49d7d72a5d6bdc21df5cd6fe93
Bjørn Fjukstad <bfj001@post.uit.no>
2013-10-02 12:59:37 +0200

    almost working, but edges do not show when force-direction is doing is job.

--- root_serve_dir/js/sigma-graph.js
+++ root_serve_dir/js/sigma-graph.js
@@ -8,13 +8,14 @@ graph
     })
     .graphProperties({
         minNodeSize: 0.5, 
-        maxNodeSize: 10,
+        maxNodeSize: 5,
         /*
         minEdgeSize: 30
         */
 });
 
 graph
+    
     .addNode('hello', {
         id: 1,
         x: 10,
@@ -24,6 +25,7 @@ graph
                       Math.round(Math.random()*256)+','+
                       Math.round(Math.random()*256)+')'
     })
+        /*
     .addNode('world', {
         id: 2,
         x: 20,
@@ -34,14 +36,21 @@ graph
                       Math.round(Math.random()*256)+')'
     })
     .addEdge('hello_world', 'hello', 'world')
+    */
     .draw()
     .startForceAtlas2(); 
+    
 
 
 // Updates from server
 var websocketUrl = "ws://" + window.location.hostname + ":3999/ws"
 var socket; 
 
+function randomColor() {
+    return 'rgb('+Math.round(Math.random()*256)+','+
+                      Math.round(Math.random()*256)+','+
+                      Math.round(Math.random()*256)+')';
+}
 
 function initWebSocket(){
 
@@ -51,25 +60,50 @@ function initWebSocket(){
             var message = JSON.parse(m.data);
             console.log("Received message:",message); 
             if(message.command == "\"InitGraph\""){
- 
+                json = JSON.parse(JSON.parse(message.graph));
+                for(var i in json.nodes) {
+                    var n = json.nodes[i];
+                    graph.addNode( n.id, {
+                        id: n.id,
+                        label: JSON.parse(n.name),
+                        x: Math.random()*100,
+                        y: Math.random()*100,
+                        color: randomColor()
+                    });
+                }
+                for (var j in json.edges) {
+                    var e = json.edges[j];
+                    graph.addEdge( e.source, e.target, e.weight );
+                }
+
+
             }
             if(message.command == "\"AddNode\""){
                 //graph.addNode( createNodeMap( message.id, message.name, 0, message.size ) );
-                graph.addNode( message.id, JSON.parse(message.name) );
+                graph.addNode( message.id, {
+                        id: message.id,
+                        x: Math.random()*100,
+                        y: Math.random()*100,
+                        label: JSON.parse(message.name),
+                        color: randomColor()
+                        
+                    });
             }
             if(message.command == "\"AddEdge\""){
                 //graph.addLink( createEdgeMap( message.source, message.target, message.id, message.weight ) );
-                graph.addLink( message.source, message.target, message.weight );
+                graph.addEdge( message.source, message.target, message.weight );
             }
             if (message.command == "\"RemoveNode\"") {
-                graph.removeNode(message.id)
+                graph.dropNode(message.id)
             }
             if (message.command == "\"RemoveEdge\"") {
                 source = '' + message.source;
                 target = '' + message.target;
                 id = '' + message.id;
-                graph.removeLink( source, target );
+                graph.dropEdge( source, target );
             }
+
+            graph.startForceAtlas2();
 
         }
     }
